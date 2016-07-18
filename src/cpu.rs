72998280835207929855432b06d248300fe8df65
Andrei Faur <da3drus@gmail.com>
2016-07-18 14:30:16 +0200

    Add CPU modelling

---
+++ src/cpu.rs
@@ -0,0 +1,21 @@
+#[derive(Default, Debug)]
+pub struct Cpu {
+    af: u16,
+    bc: u16,
+    de: u16,
+    hl: u16,
+    af_: u16,
+    bc_: u16,
+    de_: u16,
+    hl_: u16,
+    ir: u16,
+    ix: u16,
+    iy: u16,
+    sp: u16,
+    pc: u16
+}
+
+impl Cpu {
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod cpu;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut cpu = cpu::Cpu::default();
+    println!("{:?}", cpu);
 }
