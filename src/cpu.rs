d5738a4d7090c0a73762d6b296414362e8629def
Andrei Faur <da3drus@gmail.com>
2016-08-14 21:04:46 +0200

    Add possibility of selecting registers in output

--- src/cpu.rs
+++ src/cpu.rs
@@ -78,6 +78,35 @@ bitflags! {
     }
 }
 
+bitflags! {
+    pub flags OutputRegisters: u32 {
+        const OA = 0x00000001,
+        const OF = 0x00000002,
+        const OB = 0x00000004,
+        const OC = 0x00000008,
+        const OD = 0x00000010,
+        const OE = 0x00000020,
+        const OH = 0x00000040,
+        const OL = 0x00000080,
+
+        const OA_ALT = 0x00000100,
+        const OF_ALT = 0x00000200,
+        const OB_ALT = 0x00000400,
+        const OC_ALT = 0x00000800,
+        const OD_ALT = 0x00001000,
+        const OE_ALT = 0x00002000,
+        const OH_ALT = 0x00004000,
+        const OL_ALT = 0x00008000,
+
+        const OIX = 0x00010000,
+        const OIY = 0x00020000,
+        const OSP = 0x00040000,
+        const OPC = 0x00080000,
+
+        const OALL = 0xFFFFFFFF,
+    }
+}
+
 pub struct Cpu {
     // main register set
     a: u8, f: StatusIndicatorFlags,
@@ -121,42 +150,7 @@ pub struct Cpu {
 
 impl fmt::Debug for Cpu {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "
-                                               ------------
-                                               | SZ_H_PNC |
-                                            f: | {:08b} |
-                                               ------------
-
-                         -----------           -----------
-                     af: | {:02X} | {:02X} |   af_alt: | {:02X} | {:02X} |
-                     bc: | {:02X} | {:02X} |   bc_alt: | {:02X} | {:02X} |
-                     de: | {:02X} | {:02X} |   de_alt: | {:02X} | {:02X} |
-                     hl: | {:02X} | {:02X} |   hl_alt: | {:02X} | {:02X} |
-                         -----------           -----------
-
-                         -----------
-                     ir: | {:02X} | {:02X} |
-                         -----------
-
-                         ----------
-                     ix: |  {:04X}  |
-                     iy: |  {:04X}  |
-                     sp: |  {:04X}  |
-                     pc: |  {:04X}  |
-                         ----------
-
-                     {:?}",
-                      self.f.bits(),
-                      self.a, self.f.bits() as u8, self.a_alt, self.f_alt.bits() as u8,
-                      self.b, self.c, self.b_alt, self.c_alt,
-                      self.d, self.e, self.d_alt, self.e_alt,
-                      self.h, self.l, self.h_alt, self.l_alt,
-                      self.i, self.r,
-                      self.ix,
-                      self.iy,
-                      self.sp,
-                      self.pc,
-                      self.memory)
+        writeln!(f, "{}", self.output(OALL))
     }
 }
 
@@ -183,6 +177,60 @@ impl Cpu {
 
             memory: memory
         }
+    }
+
+    pub fn output(&self, regs: OutputRegisters) -> String {
+        let mut outstr = String::new();
+
+        let astr = if regs.contains(OA) { format!(" {:02X} ", self.a) } else { String::from("    ") };
+        let fstr = if regs.contains(OF) { format!(" {:02X} ", self.f.bits() as u8) } else { String::from("    ") };
+        let fbinstr = if regs.contains(OF) { format!(" {:08b} ", self.f.bits()) } else { String::from("          ") };
+        let aaltstr = if regs.contains(OA_ALT) { format!(" {:02X} ", self.a_alt) } else { String::from("    ") };
+        let faltstr = if regs.contains(OF_ALT) { format!(" {:02X} ", self.f_alt.bits() as u8) } else { String::from("    ") };
+        let bstr = if regs.contains(OB) { format!(" {:02X} ", self.b) } else { String::from("    ") };
+        let cstr = if regs.contains(OC) { format!(" {:02X} ", self.c) } else { String::from("    ") };
+        let baltstr = if regs.contains(OB_ALT) { format!(" {:02X} ", self.b_alt) } else { String::from("    ") };
+        let caltstr = if regs.contains(OC_ALT) { format!(" {:02X} ", self.c_alt) } else { String::from("    ") };
+        let dstr = if regs.contains(OD) { format!(" {:02X} ", self.d) } else { String::from("    ") };
+        let estr = if regs.contains(OE) { format!(" {:02X} ", self.e) } else { String::from("    ") };
+        let daltstr = if regs.contains(OD_ALT) { format!(" {:02X} ", self.d_alt) } else { String::from("    ") };
+        let ealtstr = if regs.contains(OE_ALT) { format!(" {:02X} ", self.e_alt) } else { String::from("    ") };
+        let hstr = if regs.contains(OH) { format!(" {:02X} ", self.h) } else { String::from("    ") };
+        let lstr = if regs.contains(OL) { format!(" {:02X} ", self.l) } else { String::from("    ") };
+        let haltstr = if regs.contains(OH_ALT) { format!(" {:02X} ", self.h_alt) } else { String::from("    ") };
+        let laltstr = if regs.contains(OL_ALT) { format!(" {:02X} ", self.l_alt) } else { String::from("    ") };
+        let ixstr = if regs.contains(OIX) { format!(" {:04X} ", self.ix) } else { String::from("      ") };
+        let iystr = if regs.contains(OIY) { format!(" {:04X} ", self.iy) } else { String::from("      ") };
+        let spstr = if regs.contains(OSP) { format!(" {:04X} ", self.sp) } else { String::from("      ") };
+        let pcstr = if regs.contains(OPC) { format!(" {:04X} ", self.pc) } else { String::from("      ") };
+
+        outstr.push_str("                    -----------           -----------\n");
+        outstr.push_str("                af: |"); outstr.push_str(&astr); outstr.push_str("|"); outstr.push_str(&fstr);
+        outstr.push_str("|   af_alt: |"); outstr.push_str(&aaltstr); outstr.push_str("|"); outstr.push_str(&faltstr);
+        outstr.push_str("|\n");
+        outstr.push_str("                bc: |"); outstr.push_str(&bstr); outstr.push_str("|"); outstr.push_str(&cstr);
+        outstr.push_str("|   bc_alt: |"); outstr.push_str(&baltstr); outstr.push_str("|"); outstr.push_str(&caltstr);
+        outstr.push_str("|\n");
+        outstr.push_str("                de: |"); outstr.push_str(&dstr); outstr.push_str("|"); outstr.push_str(&estr);
+        outstr.push_str("|   de_alt: |"); outstr.push_str(&daltstr); outstr.push_str("|"); outstr.push_str(&ealtstr);
+        outstr.push_str("|\n");
+        outstr.push_str("                hl: |"); outstr.push_str(&hstr); outstr.push_str("|"); outstr.push_str(&lstr);
+        outstr.push_str("|   hl_alt: |"); outstr.push_str(&haltstr); outstr.push_str("|"); outstr.push_str(&laltstr);
+        outstr.push_str("|\n");
+        outstr.push_str("                    -----------           -----------\n");
+        outstr.push_str("                    ----------            ------------\n");
+        outstr.push_str("                ix: | "); outstr.push_str(&ixstr);
+        outstr.push_str(" |            | SZ_H_PNC |\n");
+        outstr.push_str("                iy: | "); outstr.push_str(&iystr);
+        outstr.push_str(" |         f: |"); outstr.push_str(&fbinstr);
+        outstr.push_str("|\n");
+        outstr.push_str("                sp: | "); outstr.push_str(&spstr);
+        outstr.push_str(" |            ------------\n");
+        outstr.push_str("                pc: | "); outstr.push_str(&pcstr);
+        outstr.push_str(" |\n");
+        outstr.push_str("                    ----------\n");
+
+        outstr
     }
 
     pub fn run(&mut self) {
