02091508f5b8a590f31cbc24293ff64860b997b7
Andrei Faur <da3drus@gmail.com>
2016-09-25 12:58:37 +0200

    Refactor Memory struct to get rid of warnings

--- src/memory.rs
+++ src/memory.rs
@@ -3,9 +3,9 @@ use std::fmt;
 #[derive(RustcEncodable, RustcDecodable)]
 pub struct Memory {
     rom: u8,
-    ram_0x4000_0x7FFF: usize,
-    ram_0x8000_0xBFFF: usize,
-    ram_0xC000_0xFFFF: usize,
+    ram_0x4000_0x7fff: usize,
+    ram_0x8000_0xbfff: usize,
+    ram_0xc000_0xffff: usize,
 
     rom0: Vec<u8>,
     rom1: Vec<u8>,
@@ -16,22 +16,22 @@ pub struct Memory {
 
 impl Memory {
     pub fn read_word(&self, addr: u16) -> u8 {
-        let mut val = 0;
-        if addr <= 0x3FFF {
-            match self.rom {
-                0 => { val = self.rom0[addr as usize]; },
-                1 => { val = self.rom1[addr as usize]; },
-                _ => unreachable!()
+        let val = match addr {
+            0x0000...0x3FFF => {
+                match self.rom {
+                    0 => self.rom0[addr as usize],
+                    1 => self.rom1[addr as usize],
+                    _ => unreachable!()
+                }
             }
-        } else if addr >= 0x4000 && addr <= 0x7FFF {
-            val = self.bank[self.ram_0x4000_0x7FFF][(addr - 0x4000) as usize];
-        } else if addr >= 0x8000 && addr <= 0xBFFF {
-            val = self.bank[self.ram_0x8000_0xBFFF][(addr - 0x8000) as usize];
-        } else if addr >= 0xC000 {
-            val = self.bank[self.ram_0xC000_0xFFFF][(addr - 0xC000) as usize];
-        } else {
-            panic!("Trying to read from unrecognized address: {:#x}", addr);
-        }
+            0x4000...0x7FFF =>
+                self.bank[self.ram_0x4000_0x7fff][(addr - 0x4000) as usize],
+            0x8000...0xBFFF =>
+                self.bank[self.ram_0x8000_0xbfff][(addr - 0x8000) as usize],
+            0xC000...0xFFFF =>
+                self.bank[self.ram_0xc000_0xffff][(addr - 0xC000) as usize],
+            _ => unreachable!()
+        };
 
         debug!("                Read value {:#04X} from address {:#06X}", val, addr);
         val
@@ -39,11 +39,11 @@ impl Memory {
 
     pub fn write_word(&mut self, addr: u16, val: u8) {
         if addr >= 0xC000 {
-            self.bank[self.ram_0xC000_0xFFFF][(addr - 0xC000) as usize] = val;
+            self.bank[self.ram_0xc000_0xffff][(addr - 0xC000) as usize] = val;
         } else if addr >= 0x8000 && addr <= 0xBFFF {
-            self.bank[self.ram_0x8000_0xBFFF][(addr - 0x8000) as usize] = val;
+            self.bank[self.ram_0x8000_0xbfff][(addr - 0x8000) as usize] = val;
         } else if addr >= 0x4000 && addr <= 0x7FFF {
-            self.bank[self.ram_0x4000_0x7FFF][(addr - 0x4000) as usize] = val;
+            self.bank[self.ram_0x4000_0x7fff][(addr - 0x4000) as usize] = val;
         } else {
             if self.writable_rom {
                 match self.rom {
@@ -60,7 +60,7 @@ impl Memory {
     }
 
     pub fn change_bank(&mut self, val: u8) {
-        self.ram_0xC000_0xFFFF = val as usize;
+        self.ram_0xc000_0xffff = val as usize;
     }
 
     pub fn change_rom_bank(&mut self, val: u8) {
@@ -70,23 +70,23 @@ impl Memory {
 
 impl fmt::Debug for Memory {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "ram_0xC000_0xFFFF: {}
-                     ram_0x8000_0xBFFF: {}
-                     ram_0x4000_0x7FFF: {}
+        writeln!(f, "ram_0xc000_0xffff: {}
+                     ram_0x8000_0xbfff: {}
+                     ram_0x4000_0x7fff: {}
                      rom: {}
                     ",
-                    self.ram_0xC000_0xFFFF,
-                    self.ram_0x8000_0xBFFF,
-                    self.ram_0x4000_0x7FFF,
+                    self.ram_0xc000_0xffff,
+                    self.ram_0x8000_0xbfff,
+                    self.ram_0x4000_0x7fff,
                     self.rom)
     }
 }
 
 pub struct MemoryBuilder {
     rom: u8,
-    ram_0x4000_0x7FFF: usize,
-    ram_0x8000_0xBFFF: usize,
-    ram_0xC000_0xFFFF: usize,
+    ram_0x4000_0x7fff: usize,
+    ram_0x8000_0xbfff: usize,
+    ram_0xc000_0xffff: usize,
 
     rom0: Vec<u8>,
     rom1: Vec<u8>,
@@ -99,9 +99,9 @@ impl MemoryBuilder {
     pub fn new() -> MemoryBuilder {
         MemoryBuilder {
             rom: 0,
-            ram_0x4000_0x7FFF: 5,
-            ram_0x8000_0xBFFF: 2,
-            ram_0xC000_0xFFFF: 0,
+            ram_0x4000_0x7fff: 5,
+            ram_0x8000_0xbfff: 2,
+            ram_0xc000_0xffff: 0,
 
             rom0: vec![0; 16 * 1024],
             rom1: vec![0; 16 * 1024],
@@ -139,9 +139,9 @@ impl MemoryBuilder {
     pub fn finalize(self) -> Memory {
         Memory {
             rom: self.rom,
-            ram_0x4000_0x7FFF: self.ram_0x4000_0x7FFF,
-            ram_0x8000_0xBFFF: self.ram_0x8000_0xBFFF,
-            ram_0xC000_0xFFFF: self.ram_0xC000_0xFFFF,
+            ram_0x4000_0x7fff: self.ram_0x4000_0x7fff,
+            ram_0x8000_0xbfff: self.ram_0x8000_0xbfff,
+            ram_0xc000_0xffff: self.ram_0xc000_0xffff,
 
             rom0: self.rom0,
             rom1: self.rom1,
