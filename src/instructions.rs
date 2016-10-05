3211ff6213681b47339066513c35208ba4168517
Andrei Faur <da3drus@gmail.com>
2016-10-05 20:19:19 +0200

    Add memory contention for dec_ss instruction

--- src/instructions.rs
+++ src/instructions.rs
@@ -1198,6 +1198,10 @@ impl Instruction for DecSs {
         let r   = cpu.read_reg16(self.r);
         let res = r.wrapping_sub(1);
 
+        let pc = cpu.get_pc() + 1;
+        cpu.contend_read_no_mreq(pc);
+        cpu.contend_read_no_mreq(pc);
+
         cpu.write_reg16(self.r, res);
 
         info!("{:#06x}: DEC {:?}", cpu.get_pc(), self.r);
