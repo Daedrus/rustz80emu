8b7af018cc8743022cde196657a2f9a5b619d336
Andrei Faur <da3drus@gmail.com>
2017-08-05 20:44:32 +0200

    Allow Window to be closed

--- src/machine.rs
+++ src/machine.rs
@@ -12,6 +12,8 @@ use std::cell::RefCell;
 extern crate sdl2;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
 pub struct Machine {
     cpu: Rc<RefCell<Cpu>>,
@@ -67,7 +69,18 @@ impl Machine {
         let mut texture = texture_creator.create_texture_streaming(
             PixelFormatEnum::RGB24, 256, 192).unwrap();
 
-        loop {
+        let mut event_pump = sdl_context.event_pump().unwrap();
+
+        'machine: loop {
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        break 'machine
+                    },
+                    _ => {}
+                }
+            }
+
             if self.debug_on { debugger.pre(); }
 
             self.cpu.borrow_mut().handle_interrupts();
